ec2603da391a9efb4b4f56defda16b2a1002b1c5
Alexandre <alexandrefornari@gmail.com>
2012-03-14 14:16:44 -0300

    Inserção da pasta deployment.

---
+++ deployment/js/ai-config.js
@@ -0,0 +1,15 @@
+// Variáveis do AI_Loader
+var flashvars = {};
+flashvars.ai = "swf/AI-0091.swf";
+flashvars.width = "700";
+flashvars.height = "450";
+
+// Parâmetros do player (Flash)
+var params = {};
+params.menu = "false";
+params.scale = "noscale";
+
+// Atributos da tag HTML que contém o SWF
+var attributes = {};
+attributes.id = "ai";
+attributes.align = "middle";
